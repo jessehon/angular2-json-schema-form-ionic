f1ff861eddbf3a2f09f182f1d4f5abd1956a4698
Jorge Pujalte <jorge.pujalte@ventia.com.au>
2017-12-12 11:23:02 +1100

    Added ionic checkbox widget

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { IonicButtonComponent } from './ionic-button.component';
 export { IonicInputComponent } from './widgets/input/ionic-input.component';
+export { IonicCheckboxComponent } from './widgets/checkbox/ionic-checkbox.component';
 export { IonicFrameworkComponent } from './ionic-framework.component';
 export { IonicFrameworkModule, IONIC_FRAMEWORK_COMPONENTS } from './ionic-framework.module';
--- src/ionic-framework.module.ts
+++ src/ionic-framework.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { JsonSchemaFormService, WidgetLibraryModule } from 'angular2-json-schema-form';
 import { IonicModule } from 'ionic-angular';
 import { IonicButtonComponent } from './ionic-button.component';
-import { IonicInputComponent } from './widgets/input/ionic-input.component'
+import { IonicInputComponent } from './widgets/input/ionic-input.component';
+import { IonicCheckboxComponent } from './widgets/checkbox/ionic-checkbox.component'
 import { IonicFrameworkComponent } from './ionic-framework.component';
 
 export const IONIC_FRAMEWORK_COMPONENTS = [
   IonicButtonComponent,
   IonicInputComponent,
+  IonicCheckboxComponent,
   IonicFrameworkComponent,
 ];
 
